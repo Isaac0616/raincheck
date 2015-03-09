c026f68f9d5ddadfb7711122dc63aea58e0d19ec
Isaac <b00902067@ntu.edu.tw>
2015-03-10 00:38:54 +0800

    calculate time spend

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-from time import sleep
+from time import time
 import random
 from sympy import factorint
 
@@ -12,16 +12,20 @@ rc = RainCheck(name='all', queue_size=3, time_pause=1, time_interval=10, threads
 @rc.raincheck()
 def index():
     prime = int(request.args.get('p', ''))
+    ts = time()
     factors = factorint(prime)
+    time_spend = time() - ts
 
-    return 'Ans: ' + ' + '.join([str(f) + '^' + str(e) for f, e in factors.iteritems()])
+    return 'Time spend: ' + str(time_spend) + '<br>Ans: ' + ' + '.join([str(f) + '^' + str(e) for f, e in factors.iteritems()])
 
 @app.route('/prime')
 def hello():
     prime = int(request.args.get('p', ''))
+    ts = time()
     factors = factorint(prime)
+    time_spend = time() - ts
 
-    return 'Ans: ' + ' + '.join([str(f) + '^' + str(e) for f, e in factors.iteritems()])
+    return 'Time spend: ' + str(time_spend) + '<br>Ans: ' + ' + '.join([str(f) + '^' + str(e) for f, e in factors.iteritems()])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
