157dfdce1d22ffd999ed271eff6c837a4110f0c5
Isaac <b00902067@ntu.edu.tw>
2015-03-10 16:42:34 +0800

    adding time limit to factorization

--- hello.py
+++ hello.py
@@ -2,18 +2,32 @@ from flask import Flask, request
 app = Flask(__name__)
 
 from time import time
-import random
 from sympy import factorint
+from multiprocessing import Process, Queue
 
 from raincheck import RainCheck
 rc = RainCheck(name='all', queue_size=3, time_pause=1, time_interval=10, threads=1, key='this is secret key')
+
+
+def factor(q, prime):
+    q.put(factorint(prime))
 
 @app.route('/rc_prime')
 @rc.raincheck()
 def index():
     prime = int(request.args.get('p', ''))
+
     ts = time()
-    factors = factorint(prime)
+
+    q = Queue()
+    p = Process(target=factor, args=(q, prime))
+    p.start()
+    try:
+        factors = q.get(timeout=30)
+    except:
+        p.terminate()
+        return 'Time out'
+
     time_spend = time() - ts
 
     return 'Time spend: ' + str(time_spend) + '<br>Ans: ' + ' + '.join([str(f) + '^' + str(e) for f, e in factors.iteritems()])
@@ -21,8 +35,18 @@ def index():
 @app.route('/prime')
 def hello():
     prime = int(request.args.get('p', ''))
+
     ts = time()
-    factors = factorint(prime)
+
+    q = Queue()
+    p = Process(target=factor, args=(q, prime))
+    p.start()
+    try:
+        factors = q.get(timeout=30)
+    except:
+        p.terminate()
+        return 'Time out'
+
     time_spend = time() - ts
 
     return 'Time spend: ' + str(time_spend) + '<br>Ans: ' + ' + '.join([str(f) + '^' + str(e) for f, e in factors.iteritems()])
